66e48ffb17047a874b26673bb3fbf332e29dba2b
Jakub-C-7 <jakubchamera7@gmail.com>
2023-03-19 23:18:40 +0000

    Created another version of CollectableItem for weapons

---
+++ Assets/Scripts/CollectableWeapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableWeapon : CollectableItem
+{
+    // Damage structure
+    public int[] damageAmount;
+    public float[] pushForce;
+
+    // Weapon Upgrade
+    public int weaponLevel;
+
+}
