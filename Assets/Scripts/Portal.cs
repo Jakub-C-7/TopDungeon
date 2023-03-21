04250cc2a6abe683a7992b37e1dfb04515f95bc2
Jakub-C-7 <jakubchamera7@gmail.com>
2023-03-21 22:39:16 +0000

    Portals prompt for interaction and new portal variant created

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,17 +6,30 @@ using UnityEngine;
 public class Portal : Collidable
 {
     public string[] sceneNames;
+    public Animator portalAnimator;
 
     protected override void OnCollide(Collider2D coll)
     {
+
         if (coll.name == "Player")
         {
-            GameManager.instance.player.SetReduceLight(true);
+            GameManager.instance.ShowText("E", 25, Color.white, transform.position + new Vector3(0, 0.08f, 0), Vector3.zero, 0);
 
-            GameManager.instance.player.canMove = false;
+            if (coll.name == "Player" && Input.GetKeyDown(KeyCode.E))
+            {
+                if (portalAnimator)
+                {
+                    portalAnimator.SetTrigger("Open");
+                }
 
-            Invoke("Teleport", 0.5f);
+                GameManager.instance.player.SetReduceLight(true);
+
+                GameManager.instance.player.canMove = false;
+
+                Invoke("Teleport", 0.5f);
+            }
         }
+
     }
 
     private void Teleport()
