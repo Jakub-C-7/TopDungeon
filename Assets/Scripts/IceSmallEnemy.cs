887e462240c1616b78a2f981218765d2c55def09
FreyaRhianna <freyarsh@gmail.com>
2023-03-26 23:21:27 +0100

    Added enemy projectile capability

--- Assets/Scripts/IceSmallEnemy.cs
+++ Assets/Scripts/IceSmallEnemy.cs
@@ -7,10 +7,21 @@ public class IceSmallEnemy : Enemy
 
     public float attackCooldown = 1.5f;
     public float lastAttack;
+    GameObject projectile;
+    Transform playerTransform;
+    public int damageAmount;
+    public float pushForce;
+    public float range;
     protected override void Start()
     {
         base.Start();
+        projectile = GameManager.instance.prefabList.Find(x => x.name.Equals("blue_sweet"));
+        Projectile projectileComponent = projectile.GetComponent<Projectile>();
+        projectileComponent.damageAmount = damageAmount;
+        projectileComponent.pushForce = pushForce;
+        projectileComponent.origin = transform.name;
         lastAttack = Time.time - attackCooldown;
+        playerTransform = GameObject.Find("Player").transform;
       
         stateMachine.stateMapper = new Dictionary<EnemyStatePhases, IState>{
             [EnemyStatePhases.Idle] = new IdleState(),
@@ -30,8 +41,10 @@ public class IceSmallEnemy : Enemy
 
     public override void LaunchProjectile(){
         if(Time.time - lastAttack > attackCooldown){
-            Debug.Log("Launch projectile");
             lastAttack = Time.time;
+            GameObject projectileInstance = Instantiate(projectile, transform.position, Quaternion.identity);
+            projectileInstance.GetComponent<Rigidbody2D>().velocity = (playerTransform.position - transform.position).normalized;
+            Destroy(projectileInstance, range);
 
         }
     }
