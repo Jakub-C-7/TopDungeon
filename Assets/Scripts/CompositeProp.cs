ee4f5d0ef8d71753fe5d541361b544aa14c522e3
FreyaRhianna <freyarsh@gmail.com>
2023-04-24 21:02:56 +0100

    Added group placement of composite groups

--- Assets/Scripts/CompositeProp.cs
+++ Assets/Scripts/CompositeProp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class PropGroup : Prop
+public class CompositeProp : Prop
 {
     public List<Prop> propsInGroup;
 }
